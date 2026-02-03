bb21475d0a86dd57bd31732240e90cd0d70ad602
NikhilSinghRajaputana <59113685+NikhilSinghRajaputana@users.noreply.github.com>
2026-02-03 16:16:40 +0530

    Dock

---
+++ Docker/react-app/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+
+
+      </div>
+      
+      <h1>Hello React from Docker (soon)</h1>
+       <h1>Vershion 2.0</h1>
+      <div className="card">
+       
+       
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
